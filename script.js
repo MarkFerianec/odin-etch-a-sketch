2f65bbd66600c763af63071d08f4a6546a915db6
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-06-24 00:21:39 -0600

    Modify index, script and style files.
    The container is a static size regardless of how many squares
    are present in the etch-a-sketch screen. For example if the
    container is 400 pixels by 400 pixels, it remains those dimensions
    if the grid is 16 by 16 or 100 by 100.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const gridContainerDiv = document.querySelector('.gridContainerDiv');
-const row1container = document.querySelector('.row1container');
-const row2container = document.querySelector('.row2container');
-const row3container = document.querySelector('.row3container');
+// const gridContainerDiv = document.querySelector('.gridContainerDiv');
+// const row1container = document.querySelector('.row1container');
+// const row2container = document.querySelector('.row2container');
+// const row3container = document.querySelector('.row3container');
 
 const gridContainerDiv2 = document.querySelector('.gridContainerDiv2');
 // for loop through an array and assign the ith value of the array
@@ -24,26 +24,26 @@ const gridContainerDiv2 = document.querySelector('.gridContainerDiv2');
 
 // console.log(divArray);
 
-let size = 3;
+let size = 16;
 
-for (let i = 0; i < size; i++) {
-    let row1 = document.createElement('div');
-    row1.textContent = 'row,';
-    row1container.appendChild(row1);
-}
+// for (let i = 0; i < size; i++) {
+//     let row1 = document.createElement('div');
+//     row1.textContent = 'row,';
+//     row1container.appendChild(row1);
+// }
 
-for (let i = 0; i < size; i++) {
-    let row2 = document.createElement('div');
-    row2.textContent = 'row2,';
-    row2container.appendChild(row2);
+// for (let i = 0; i < size; i++) {
+//     let row2 = document.createElement('div');
+//     row2.textContent = 'row2,';
+//     row2container.appendChild(row2);
 
-}
+// }
 
-for (let i = 0; i < size; i++) {
-    let row3 = document.createElement('div');
-    row3.textContent = 'row3,';
-    row3container.appendChild(row3);
-}
+// for (let i = 0; i < size; i++) {
+//     let row3 = document.createElement('div');
+//     row3.textContent = 'row3,';
+//     row3container.appendChild(row3);
+// }
 
 // gridContainerDiv2 container
 // for (let i = 0; i < size; i++) {
@@ -52,25 +52,62 @@ for (let i = 0; i < size; i++) {
 //     console.log(container);
 // }
 
-
 for (let i = 0; i < size; i++) {
+
     let container = document.createElement('div');
+    // container.style.maxWidth = '300px';         //This is probably targeting wrong
+    // container.style.maxHeight = '300px';        //containers right?
+    // container.style.alignSelf = 'center';
+    container.style.display = 'flex';
+    container.style.flexDirection = 'column';
+    container.style.flex = '1'; //might be incorrect value(s) for grow, shrink and basis
     gridContainerDiv2.appendChild(container);
-    console.log(container);
+
     let row = document.createElement('div');
-    row.textContent = 'row,+';
+    row.style.border = 'solid 1px';
+    // row.style.padding = '10px';
+    row.style.display = 'flex';
+    row.style.flex = '1';          // added this
+    // row.style.maxWidth = '15px';
+    // row.style.maxHeight = '15px';
+    row.addEventListener('mouseover', () => {
+        row.style.backgroundColor = 'black';
+    });
     container.appendChild(row);
     for (let j = 0; j < size - 1; j++) {
         let row = document.createElement('div');
-        row.textContent = 'row,-';
+        row.style.border = 'solid 1px';
+        // row.style.padding = '10px';
+        row.style.display = 'flex';
+        row.style.flex = '1'; //might be wrong   // added this
+        row.addEventListener('mouseover', () => {
+            row.style.backgroundColor = 'black';
+        });
         container.appendChild(row);
     }
 }
 
-
-
-
-
+// for (let i = 0; i < size; i++) {
+//     let container = document.createElement('div');
+//     gridContainerDiv2.appendChild(container);
+//     console.log(container);
+//     let row = document.createElement('div');
+//     row.textContent = '1';
+//     container.appendChild(row);
+//     for (let j = 0; j < size - 1; j++) {
+//         let row = document.createElement('div');
+//         // row.textContent = '1';
+//         row.style.border = 'solid 1px';
+//         row.style.padding = '10px';
+//         // row.onmouseover = 'black';
+//         // row.style['onmouseover'] = 'black';
+//         // row.style.onmouseover = 'black';
+//         row.addEventListener('mouseover', () => {
+//             row.style.backgroundColor = 'black';
+//         });
+//         container.appendChild(row);
+//     }
+// }
 
 // for (let i = 0; i < size; i++) {
 //     let row = document.createElement('div');
@@ -82,14 +119,6 @@ for (let i = 0; i < size; i++) {
 //         gridContainerDiv.appendChild(row);
 //     }
 // }
-
-
-
-
-
-
-
-
 
 
 // for (let i = 0; i < divArray.length; i++) {
@@ -105,6 +134,7 @@ for (let i = 0; i < size; i++) {
 //     }
 // }
 
+
 // for (let i = 0; i < divArray.length; i++) {
 //     let row = document.createElement('div');
 //     row.textContent = 'row,'
@@ -117,6 +147,7 @@ for (let i = 0; i < size; i++) {
 //     column.style.color = 'red';
 //     gridColumns.appendChild(column);
 // }
+
 
 // for (let i = 0; i < divArray.length; i++) {
 //     let row = document.createElement('div');
@@ -135,6 +166,7 @@ for (let i = 0; i < size; i++) {
 //     }
 // }
 
+
 // for (let i = 0; i < divArray.length + 1; i++) {
 //     let row = document.createElement('div');
 //     row.textContent = 'row'
@@ -150,13 +182,16 @@ for (let i = 0; i < size; i++) {
 //     }
 // }
 
+
 // for (let i = 0; i < 5; i++) {
 //     console.log(i)
 // }
 
+
 // for (let j = 0; j < divArray.length + 1; j++) {
 //     console.log(j);
 // }
+
 
 // We are going to write code that loops through a length of an array
 // then for the length we will make divs that matches what user inputs
