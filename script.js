585d07cd3b297d9554d5224876d1ea67e6861023
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-06-25 02:54:35 -0600

    Modify script file
    Added grid dimensions button functionality and modified existing
    code.

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 const gridContainerDiv = document.querySelector('.gridContainerDiv');
+const gridDimensionsPromptButton = document.querySelector('.gridDimensionsPromptButton');
 
-let size = 100;
+let size = 4;
 
-for (let i = 0; i < size; i++) {
+function gridGenerator() {
 
-    let container = document.createElement('div');
-    container.style.display = 'flex';
-    container.style.flexDirection = 'column';
-    container.style.flex = '1';
-    gridContainerDiv.appendChild(container);
+    removeAllChildNodes(gridContainerDiv);
 
-    let row = document.createElement('div');
-    row.style.display = 'flex';
-    row.style.flex = '1';
-    row.addEventListener('mouseover', () => {
-        row.style.backgroundColor = 'black';
-    });
-    container.appendChild(row);
-    for (let j = 0; j < size - 1; j++) {
+    size = Number(prompt("Enter size (4 to 100)"));
+
+    for (let i = 0; i < size; i++) {
+
+        let container = document.createElement('div');
+        container.style.display = 'flex';
+        container.style.flexDirection = 'column';
+        container.style.flex = '1';
+        gridContainerDiv.appendChild(container);
+
         let row = document.createElement('div');
         row.style.display = 'flex';
         row.style.flex = '1';
@@ -25,5 +24,22 @@ for (let i = 0; i < size; i++) {
             row.style.backgroundColor = 'black';
         });
         container.appendChild(row);
+        for (let j = 0; j < size - 1; j++) {
+            let row = document.createElement('div');
+            row.style.display = 'flex';
+            row.style.flex = '1';
+            row.addEventListener('mouseover', () => {
+                row.style.backgroundColor = 'black';
+            });
+            container.appendChild(row);
+        }
     }
 }
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+gridDimensionsPromptButton.addEventListener('click', gridGenerator);
